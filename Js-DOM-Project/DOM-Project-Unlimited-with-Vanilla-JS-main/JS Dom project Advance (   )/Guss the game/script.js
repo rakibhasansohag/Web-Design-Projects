82640c9c51fff62bbdd42252847a104dc610d8b2
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-03-24 02:02:53 +0600

    guess the game add with some minor change

---
+++ Js-DOM-Project/DOM-Project-Unlimited-with-Vanilla-JS-main/JS Dom project Advance (   )/Guss the game/script.js
@@ -0,0 +1,59 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+	document.querySelector('.message').textContent = message;
+};
+
+const check = document.querySelector('.check');
+check.addEventListener('click', () => {
+	const guess = Number(document.querySelector('.guess').value);
+
+	// when there is no number in the input field
+
+	if (!guess) {
+		displayMessage('please enter a number');
+	} else if (guess === secretNumber) {
+		displayMessage('congregations you got the number');
+		document.querySelector('.number').textContent = secretNumber;
+		document.querySelector('body').style.backgroundColor = '#60b347';
+		document.querySelector('.number').style.width = '30rem';
+
+		if (score > highScore) {
+			highScore = score;
+			document.querySelector('.highScore').textContent = highScore;
+		}
+		// when the guess number is wrong
+	} else if (guess !== secretNumber) {
+		if (score > 1) {
+			displayMessage(guess > secretNumber ? 'too high' : 'too low');
+			score--;
+			document.querySelector('.score').textContent = score;
+
+			// apply shake class to body element
+			document.querySelector('body').classList.add('shake');
+			setTimeout(() => {
+				document.querySelector('body').classList.remove('shake');
+			}, 500);
+		} else {
+			displayMessage('you lost the game !! LOOSER ha ha ha');
+			document.querySelector('.score').textContent = 0;
+		}
+	}
+});
+
+const again = document.querySelector('.again');
+again.addEventListener('click', () => {
+	score = 20;
+	secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+	displayMessage('Start Guessing...');
+	document.querySelector('.score').textContent = score;
+	document.querySelector('.numbers').textContent = '?';
+	document.querySelector('.guess').value = '';
+	document.querySelector('body').style.backgroundColor = '#222';
+	document.querySelector('.number').style.width = '15rem';
+});
