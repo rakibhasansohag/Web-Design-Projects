b45f7527ccb47c3df693f6ac9cd8abb87dd4596b
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-04-08 13:52:51 +0600

    add load more button and add a style to the button and add some comments on javascript

--- mainScript.js
+++ mainScript.js
@@ -1,16 +1,19 @@
+//  point 0  : declaration of variables
 const filterContainer = document.querySelector('.gallery-filter');
 const galleryItems = document.querySelectorAll('.gallery-item');
+const gallery = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.gallery-more-btn');
 
-//  for filtering the projects
+// point 1 :  for filtering the projects
 
 filterContainer.addEventListener('click', (event) => {
 	console.log('is working');
 
 	if (event.target.classList.contains('filter-item')) {
-		// deactivate existing active 'filter-item'
+		// point 1.1:  deactivate existing active 'filter-item'
 		filterContainer.querySelector('.active').classList.remove('active');
 
-		// activate new 'filter-item'
+		// point 1.2 : activate new 'filter-item'
 		event.target.classList.add('active');
 
 		const filterValue = event.target.getAttribute('data-filter');
