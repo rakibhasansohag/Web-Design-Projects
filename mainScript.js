605a02ad03fc45a7c20b51f33215b8ef36c35621
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-04-08 14:27:33 +0600

    try to add load more function to the prjoct but failed and add a json file it will updated in the future

--- mainScript.js
+++ mainScript.js
@@ -2,7 +2,7 @@
 const filterContainer = document.querySelector('.gallery-filter');
 const galleryItems = document.querySelectorAll('.gallery-item');
 const gallery = document.querySelector('.gallery');
-const loadMoreBtn = document.querySelector('.gallery-more-btn');
+const loadMoreBtn = document.querySelector('#load-more-btn');
 
 // point 1 :  for filtering the projects
 
@@ -29,3 +29,36 @@ filterContainer.addEventListener('click', (event) => {
 		});
 	}
 });
+
+//  error not working
+// // point 2:  for load more button
+
+// let projectCount = 9;
+
+// loadMoreBtn.addEventListener('click', () => loadMoreProject());
+
+// const loadMoreProject = function (e) {
+// 	// e.preventDefault();
+
+// 	// point 2.1 : use fetch to load more projects
+
+// 	fetch('/load-more-projects')
+// 		.then((res) => res.json())
+// 		.then((data) => {
+// 			// point 2.2 : append the new Projects to the existing gallery
+// 			data.projects.forEach((project) => {
+// 				const projectImages = document.createElement('img');
+// 				projectImages.setAttribute('src', project.imgSrc);
+// 				gallery.appendChild(projectImages);
+// 			});
+
+// 			// point 2.3 : increment the project count
+
+// 			projectCount += data.projects.length;
+
+// 			// point 3 : hide the  " load more " button if there are no more projects to display
+// 			if (projectCount >= data.totalProjects) {
+// 				loadMoreBtn.style.display = 'none'; //
+// 			}
+// 		});
+// };
