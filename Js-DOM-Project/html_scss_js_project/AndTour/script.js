d95e4771851448f334256fe805e4255efadaa907
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-08-12 00:18:56 +0600

    fix nav bar error

--- Js-DOM-Project/html_scss_js_project/AndTour/script.js
+++ Js-DOM-Project/html_scss_js_project/AndTour/script.js
@@ -1,2 +1,13 @@
 'use strict';
 console.log('working...');
+
+// point : nav bar
+
+const menuButton = document.querySelector('.menu-button');
+const navLinks = document.querySelector('.nav-links');
+const navbar = document.querySelector('.navbar');
+
+menuButton.addEventListener('click', () => {
+	navLinks.classList.toggle('active');
+	navbar.classList.toggle('active');
+});
